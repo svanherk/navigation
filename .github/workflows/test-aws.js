1b0b48b7015b141c6b09de38309dd6d3d13f0f7f
Stacey Van Herk <Stacey.VanHerk@D2L.com>
2020-11-25 18:12:12 -0500

    Just try stuff

--- .github/workflows/test-aws.js
+++ .github/workflows/test-aws.js
@@ -1,1 +1,42 @@
-process.stdout.write('IT WORKS');
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const path = require('path');
+
+const getBucketCreds = async () => {
+    return new Promise((resolve, reject) => {
+        const timestamp = (new Date()).getTime();
+        const params = {
+        RoleArn: 'arn:aws:iam::661160317623:role/githubactions-visual-diff-testing',
+        RoleSessionName: `repo-visual-diff-testing-${timestamp}`
+        };
+        const sts = new AWS.STS();
+        sts.assumeRole(params, (err, data) => {
+        if (err) reject(err);
+        else {
+            resolve({
+            accessKeyId: data.Credentials.AccessKeyId,
+            secretAccessKey: data.Credentials.SecretAccessKey,
+            sessionToken: data.Credentials.SessionToken,
+            });
+        }
+        });
+    });
+}
+
+const getBucketContents = async () => {
+    const s3Config = await getBucketCreds();
+    s3Config.apiVersion = 'latest';
+    s3Config.region = 'ca-central-1';
+
+    const s3 = new AWS.S3(s3Config);
+    return s3.listObjects({
+        bucket: 'visualdiff.gaudi.d2l'
+    });
+
+}
+
+getBucketContents().then((results) => {
+    process.stdout.write(results.data.Contents);
+}).catch((err) => {
+    process.stdout.write(err);
+});
